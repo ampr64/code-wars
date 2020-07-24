91ec8ab541990430dfe129e0f042efc03a0ee75b
Alvaro Prieto <alviprieto@hotmail.com>
2020-07-24 16:15:30 -0300

    Complete kata "FindTheOddInt"

---
+++ 6 Kyu/FindTheOddInt/FindTheOddInt.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace FindTheOddInt
+{
+    public class FindTheOddInt
+    {
+        public int Do(int[] sequence)
+        {
+            var distinctNumbers = GetDistinctNumbers(sequence);
+            int result = default;
+
+            foreach (var number in distinctNumbers)
+            {
+                if (AppearsOddNumberOfTimes(number, sequence))
+                {
+                    result = number;
+                }
+            }
+            return result;
+        }
+
+        private bool AppearsOddNumberOfTimes(int target, int[] sequence) =>
+            CountOccurrences(target, sequence) % 2 == 1;
+
+        private IList<int> GetDistinctNumbers(int[] sequence)
+        {
+            var distinctNumbers = new List<int>();
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (!distinctNumbers.Contains(sequence[i]))
+                {
+                    distinctNumbers.Add(sequence[i]);
+                }
+            }
+            return distinctNumbers;
+        }
+
+        private int CountOccurrences(int target, int[] sequence)
+        {
+            var count = 0;
+            foreach (var number in sequence)
+            {
+                if (number == target)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 6 Kyu/FindTheOddInt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheOddInt
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please insert list of integers followed by commas, e.g 6,4,9,11");
+            var input = Console.ReadLine();
+            var numbers = ParseInput(input);
+            if (numbers == null)
+            {
+                Console.WriteLine("Invalid input.");
+                return;
+            }
+            var oddInt = new FindTheOddInt().Do(numbers);
+            Console.WriteLine("The odd integer in the provided list is: {0}", oddInt);
+            Console.ReadKey();
+        }
+
+        static int[] ParseInput(string input)
+        {
+            try
+            {
+                var result = input
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x.Trim()))
+                    .ToArray();
+                return result.Length > 0 ? result : null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
